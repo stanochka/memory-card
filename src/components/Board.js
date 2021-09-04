b29586c67950cefeaf58c903d355fad8625fe256
stanochka <krozzochka@gmail.com>
2021-09-04 22:53:16 +0300

    Add images and randomize initial render

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,25 @@
-import Card from './Card'
+import  { useState, useEffect } from "react";
+import Card from './Card';
 
-function Board() {
+function Board(props) {
+
+  const randomize = () => {
+    const a = new Set();
+    while(a.size !== 8) {
+      a.add(Math.floor(Math.random() * 50 + 1));
+    }
+    return [...a];
+  }
+
+  let initValue = randomize();
+  const [renderedCards, setRenderedCards] = useState(initValue);
+
+  useEffect(() => {
+    console.log(renderedCards);
+  }, [renderedCards])
+
   return <div className="Board">
-          <Card id="card1" />
-          <Card id="card2" />
-          <Card id="card3" />
+          {renderedCards.map(cardId => <Card key={cardId} id={cardId} />)}
          </div>
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 function Footer() {
   return (<footer>
-          Assignment by <a href="https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/memory-card" target="_blank">The Odin Project</a>.
-          Coded by <a href="https://github.com/stanochka">stanochka</a>.
+            <div>
+              Assignment by <a href="https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/memory-card" target="_blank" rel="noreferrer" title="The Odin Project">The Odin Project</a> |
+              Coded by <a href="https://github.com/stanochka" target="_blank" rel="noreferrer" title="stanochka">stanochka</a> |
+              Icons made by <a href="https://www.freepik.com" target="_blank" rel="noreferrer" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank" rel="noreferrer" title="Flaticon">www.flaticon.com</a>
+            </div>
           </footer>)
 }
 
